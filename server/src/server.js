3043368d6269dd9d6aa4323adec08d32395a9420
KumarSachin-1016 <kumarsachin101683@gmail.com>
2026-08-06 13:03:02 +0530

    Connect MongoDB to the server

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,18 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./database/db.js";
+
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to start server:", error);
+  });
