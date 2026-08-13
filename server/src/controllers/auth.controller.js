8f6eb740e4a86f822711aa879a2895ca6db00f03
KumarSachin-1016 <kumarsachin101683@gmail.com>
2026-08-13 21:13:31 +0530

    feat: implement JWT authentication and logout

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import generateToken from "../utils/generateToken.js";
+
+// Signup controller
 
 export const signup = async (req, res) => {
   try {
@@ -51,4 +54,87 @@ export const signup = async (req, res) => {
       message: "Internal server error",
     });
   }
+};
+
+// Login controller
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check required fields
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email and password are required",
+      });
+    }
+
+    // Find user
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    // Compare password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    const token = generateToken(user._id);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Login successful",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    console.error("Login Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+export const getMe = async (req, res) => {
+  return res.status(200).json({
+    success: true,
+    user: req.user,
+  });
+};
+
+export const logout = async (req, res) => {
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+  });
+
+  return res.status(200).json({
+    success: true,
+    message: "Logout successful",
+  });
 };
--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,8 +1,15 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signup, login, getMe, logout } from "../controllers/auth.controller.js";
+import protect from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
+
+router.post("/login", login);
+
+router.get("/me", protect, getMe);
+
+router.get("/logout", logout);
 
 export default router;
